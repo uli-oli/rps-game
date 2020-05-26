2c882cc9ae46106f61d1d27d7291bb8923b01729
Sh-Abd <shadyabdalhmed82@gmail.com>
2020-05-26 15:42:50 -0700

    initial code
    begening of rps game

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NzE0OTU2ODY0NzE0MDQ3NTUw.Xs2Nuw.gVUxrI0kk4cimMpGYChpxmTJNNM';
+bot.on('ready', () => {
+console.log('Bot in online');
+
+})
+
+bot.on('message', msg=>{
+   if ( msg.content === "Hello"){
+       msg.reply('Hello Back My Friend');
+   }
+})
+
+bot.login(token);
