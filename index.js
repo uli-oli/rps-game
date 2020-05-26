28a14c2d4eedadf89c20b60bf27e08b4ee9fabac
Sh-Abd <shadyabdalhmed82@gmail.com>
2020-05-26 16:17:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
 const { messageResponses } = require("./test.js");
-
 const token = 'NzE0OTU2ODY0NzE0MDQ3NTUw.Xs2Nuw.gVUxrI0kk4cimMpGYChpxmTJNNM';
+
 bot.on('ready', () => {
     console.log('Bot in online');
-    bot.channels.cache.get('714957511123533877').send ('The bot is online.')
-
+    bot.channels.cache.get('714957511123533877').send('The bot is online.');
 })
 
 bot.on('message', messege=>{
