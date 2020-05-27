5974a158f803284268471a5c5d53d4a258b8668b
ez1ollie <ez1ollie@yahoo.com>
2020-05-26 18:20:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,7 @@ bot.on("message", (message) => {
             break;
         case 'rps':
             message.reply('Lets play rock, paper, scissors!');
-            message.channel.send('Choose rock, paper, or scissors.');
-            message.react('✌️'+'✊'+'🖐️');
+            message.channel.send('Choose rock, paper, or scissors.').then(message.react('✌️'+'✊'+'🖐️'));
             break;
     }
 })
