65363695755617ce6ca15d2718883b4ad10aac53
ez1ollie <ez1ollie@yahoo.com>
2020-05-27 01:05:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ bot.on("message", (message) => {
             case 'date':
                 var today2 = new Date();
                 var date2 = today2.getFullYear()+'/'+(today2.getMonth()+1)+'/'+today2.getDate();
-                var time2 = today2.getHours()+":"+today2.getMinutes()+":"+today2.getSeconds()+":"+today2.getMilliseconds+" UTC";
+                var time2 = today2.getHours()+":"+today2.getMinutes()+":"+today2.getSeconds()+" UTC";
                 var date_time2 = date2+' '+time2;
                 message.reply(date_time2);
                 break;
