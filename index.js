b83cd6481bf0d48a3b3b97ac093383acbb3645e9
ez1ollie <ez1ollie@yahoo.com>
2020-05-26 17:56:02 -0700

    updated update message

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const PREFIX = '!';
 
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()+":"+today.getMilliseconds();
+var time = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()+" UTC";
 var date_time = date+' '+time;
+var update_deployed = 'The bot is now online. Updates may have been deployed.'+' '+date_time
 
 bot.on('ready', () => {
     console.log('Bot in online');
-    bot.channels.cache.get(rps_server_gen_channel).send('The bot is now online. Updates may have been deployed.'+' '+date_time);
-    //bot.channels.cache.get(rps_server_gen_channel).send(date_time);
+    bot.channels.cache.get(rps_server_gen_channel).send(update_deployed);
 })
 
 bot.on("message", (message) => {
