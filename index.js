8e946fce23f4132fb21a666602eb1cd21b9727c4
ez1ollie <ez1ollie@yahoo.com>
2020-05-26 17:44:02 -0700

    started on commands section. prefix = !

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const token2 = process.env.token2; //Different bot with same capabilities
 
 const rps_server_gen_channel = '714957511123533877' //This is the channel ID from the RPS discord
 
+const PREFIX = '!';
+
 bot.on('ready', () => {
     console.log('Bot in online');
     bot.channels.cache.get(rps_server_gen_channel).send('The bot is now online. Updates may have been deployed.');
@@ -16,6 +18,15 @@ bot.on("message", (message) => {
    if (messageResponses[message.content]){
        message.channel.send(messageResponses[message.content]);
    }
+})
+
+bot.on("message", (message) => {
+    let args = message.content.substring(PREFIX.length).split(" ");
+    switch(args[0]){
+        case 'ping':
+            message.reply('pong!')
+            break;
+    }
 })
 
 //bot.login(token);
