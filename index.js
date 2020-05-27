85f971b7b83fb3c515221d1e74eb7f8b2911e70b
ez1ollie <ez1ollie@yahoo.com>
2020-05-26 17:49:43 -0700

    appended time to update message

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const { messageResponses } = require("./message-responses.js");
-const token = process.env.token;
+const token = process.env.token; 
 const token2 = process.env.token2; //Different bot with same capabilities
-//const token = 'token id from discord developer app';
-
 const rps_server_gen_channel = '714957511123533877' //This is the channel ID from the RPS discord
-
 const PREFIX = '!';
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()+":"+today.getMilliseconds();
+var date_time = date+ ' '+time;
 
 bot.on('ready', () => {
     console.log('Bot in online');
     bot.channels.cache.get(rps_server_gen_channel).send('The bot is now online. Updates may have been deployed.');
+    bot.channels.cache.get(rps_server_gen_channel).send(date_time);
 })
 
 bot.on("message", (message) => {
